e3aeeb9f615df31636a4c3af198e68f6523c7bb0
Jennifer Wang <jhw002@gmail.com>
2021-07-24 15:12:35 -0700

    Insert all rows into the example database

--- script.py
+++ script.py
@@ -7,9 +7,11 @@ practice_file = "/Users/jenniferwang/data-ingestion/PracticeData.xlsx"
 
 def readSheet(file, sheet='Sheet1'):
     excel = pd.read_excel(practice_file, sheet_name = sheet)
-    return excel.head()
+    rows = []
+    for _, row in excel.iterrows():
+        rows.append(row)
+    return rows
 
-print(readSheet(practice_file))
 
 # Write to sqlite
 
@@ -24,14 +26,27 @@ con = sqlite3.connect('data-integration.db')
 
 # Create Table
 
-create_table_sql_statement = """CREATE TABLE IF NOT EXISTS shapes (
+create_table_sql_statement = """CREATE TABLE IF NOT EXISTS example (
 	id integer PRIMARY KEY,
-	title text NOT NULL,
-	shape text NOT NULL
+	title text NOT NULL
 );"""
 
 def executeSql(conn, statement):
-    c = conn.cursor()
-    c.execute(statement)
+    try:
+        c = conn.cursor()
+        c.execute(statement)
+        conn.commit()
+    except Exception as exception:
+        print(exception)
 
 executeSql(con, create_table_sql_statement)
+
+# Read Each line from excel, then insert into our db
+def insertRows(rows, conn):
+    for row in rows:
+        insert_row_sql_statement = "INSERT INTO example (id, title) VALUES ({id}, \"{title}\");".format(id=row.Id, title=row.Title)
+        print(insert_row_sql_statement)
+        executeSql(conn, insert_row_sql_statement)
+
+all_rows = readSheet(practice_file)
+insertRows(all_rows, con)
